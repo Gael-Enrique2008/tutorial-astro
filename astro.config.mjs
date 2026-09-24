5a13bf4f3376c85feb647422055cc485d7d75a1e
Gael <gaelenriquemedinamartinez@gmail.com>
2026-09-23 20:21:45 -0600

    fin2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astropractic.netlify.app',
+  site: 'https://astropractic.netlify.app',
+  integrations: [preact()],
 });
